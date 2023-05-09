5200ef638ef8738c7599d27a2a4a3fa4859d33f0
Monika Shakya <132267069+MonikaShakya@users.noreply.github.com>
2023-05-09 21:53:19 +0545

    Models and Routes Configuration 1

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,14 @@ const connectToMongo = require('./db');
 const express = require('express')
 const app = express()
 
-
 connectToMongo();
 const port = 3000
+
+//Available Routes
+app.use('/api/auth', require('./routes/auth'))
+
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.get('/', (req, res) => {
     res.send('Hello World! monika')
